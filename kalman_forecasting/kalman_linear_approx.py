c55a02534c65db6aefb869e1ba750d6d247970ba
eearl <eamon.sc.earl@gmail.com>
2025-04-02 11:43:40 -0400

    cleaning up the script

--- kalman_forecasting/kalman_linear_approx.py
+++ kalman_forecasting/kalman_linear_approx.py
@@ -8,6 +8,9 @@ from filterpy.kalman import KalmanFilter
 from sklearn.linear_model import LinearRegression
 import pickle
 from sklearn.preprocessing import StandardScaler
+import pickle
+import os.path
+import warnings
 
 def plot(test_set, predictions, title):
     plt.figure(figsize=(10, 5))
@@ -17,7 +20,7 @@ def plot(test_set, predictions, title):
     plt.ylabel("E. coli Level")
     plt.legend()
     plt.title(f"{title}")
-    plt.show()
+    plt.savefig("water_safety/kalman_forecasting/graphs//" + title)
 
 def get_year(x, dt_format = "%Y-%m-%d"):
     dt = datetime.strptime(x, dt_format)
@@ -41,8 +44,10 @@ def point_derivative(data):
 def get_training_set(summer_data, features):
     X_train, y_train = [], []
     for year, df in summer_data.items():
-        df['eColi_prev'] = df['eColi'].shift(1)
-        df['eColi_change_prev'] = df['eColi_change'].shift(1)
+        df2 = df.copy(deep=True)
+
+        df['eColi_prev'] = df2['eColi'].shift(1)
+        df['eColi_change_prev'] = df2['eColi_change'].shift(1)
         df.dropna(inplace=True)
         
         X_train.append(df[features].values)
@@ -53,59 +58,66 @@ def get_training_set(summer_data, features):
     
     return X_train, y_train
 
-if __name__ == '__main__':
+def linear_regression(file_path, X_train, y_train):
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as f:
+            weights = pickle.load(f)
+        print('Loading existing regressor ----------------------------------')
+        
+        reg = LinearRegression()
+        reg.intercept_ = weights[:,0]
+        reg.coef_ = weights[:, 1:]
 
-    data = pd.read_csv('water_safety/weather_data_scripts/cleaned_data/daily/cleaned_merged_toronto_city_hanlans.csv', index_col=0)
 
-    # add rate of change of e.coli
+    else:
+        reg = LinearRegression()
+        reg.fit(X_train, y_train)
+
+        coeffs = reg.coef_
+        intercept = reg.intercept_.reshape(-1,1)
+
+        # Save trained model
+        with open(file_path, "wb") as f:
+            pickle.dump(np.hstack((intercept, coeffs)), f)
+
+    return reg
+
+def kalman(data, horizon, q_noise, r_noise, f_estimate_func=linear_regression):
+
     data = point_derivative(data)
     
     input_features = ['Max Temp (°C)', 'Min Temp (°C)', 'Mean Temp (°C)', 'Total Precip (mm)', 'Heat Deg Days (°C)', 'Cool Deg Days (°C)']
     state_features = ['eColi_prev', 'eColi_change_prev'] + input_features
     
-    # TODO - separate scaling for test and train
-    # center
-    scaler = StandardScaler()
-    df_scaled = pd.DataFrame(scaler.fit_transform(data[['eColi', 'eColi_change'] + input_features]), columns=['eColi', 'eColi_change'] + input_features)
-   
-    means = scaler.mean_
-    stds = scaler.scale_
-    
-    summer_data = {year: get_series_by_year(data, year) for year in range(2007, 2025)}
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        summer_data = {year: get_series_by_year(data, year) for year in range(2007, 2025)}
 
-    test_year = sorted(summer_data.keys())[-1]  # hold out last summer
-    test_set = summer_data.pop(test_year) 
+        test_year = sorted(summer_data.keys())[-1]  # hold out last summer
+        test_set = summer_data.pop(test_year) 
+           # Predict on the held-out summer
+        df2 = test_set.copy(deep=True)
+        test_set['eColi_prev'] = df2['eColi'].shift(1)
+        test_set['eColi_change_prev'] = df2['eColi_change'].shift(1)
+        test_set.dropna(inplace=True)
 
-    # TODO: consider these things - or let Oliver's work distinguish if lagged features are useful
-    # add lagged features?
-    # features
-    # center the data? 
+        # TODO: consider these things - or let Oliver's work distinguish if lagged features are useful
+        # add lagged features?
 
-    # get training set
-    X_train, y_train = get_training_set(summer_data, state_features)
+        X_train, y_train = get_training_set(summer_data, state_features)
+        file_path = "linear_transition_coeffs.pkl"
+        reg = f_estimate_func(file_path, X_train, y_train)
 
-    # Train transition matrix F via Linear Regression
-    reg = LinearRegression()
-    reg.fit(X_train, y_train)
     F_estimated = np.eye(len(state_features))
     # set first two rows to the linear regression coefficients
     F_estimated[:2, :] = reg.coef_
 
-    # Save trained model
-    with open("kalman_state_transition_model.pkl", "wb") as f:
-        pickle.dump(F_estimated, f)
-
-    # Initialize Kalman Filter
+    # init Kalman Filter
     kf = KalmanFilter(dim_x=len(state_features), dim_z=2)  # dim_z = [eColi, rate of change]
     kf.F = F_estimated
     kf.H = np.eye(2, len(state_features))  # Observation matrix: we only observe [eColi, rate of change]
-    kf.Q *= 0.2  # Process noise
-    kf.R *= 1.0 # Measurement noise
-
-    # Predict on the held-out summer
-    test_set['eColi_prev'] = test_set['eColi'].shift(1)
-    test_set['eColi_change_prev'] = test_set['eColi_change'].shift(1)
-    test_set.dropna(inplace=True)
+    kf.Q *= q_noise  # Process noise
+    kf.R *= r_noise # Measurement noise
 
     state = test_set.iloc[0][state_features].values
     kf.x = np.array(state).reshape(-1, 1)  # Set initial state
@@ -121,5 +133,10 @@ if __name__ == '__main__':
         linear_predictions.append(reg.predict([row[state_features].values])[0,0])
         
         
-    plot(test_set, predictions, title="Kalman Filter Predictions for Summer 2024")
-    plot(test_set, linear_predictions, title="Linear Regression Predictions for Summer 2024")
+    plot(test_set, predictions, title=f"Kalman Filter - Summer {test_year} - Horizon {horizon}")
+    plot(test_set, linear_predictions, title=f"Linear Regression - Summer {test_year}")
+
+if __name__ == '__main__':
+
+    data = pd.read_csv('water_safety/weather_data_scripts/cleaned_data/daily/cleaned_merged_toronto_city_hanlans.csv', index_col=0)
+    kalman(data, 1, 0.2, 1)
